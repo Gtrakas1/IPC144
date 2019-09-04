8a0554b10e193cccf229af0c1b45e079ba56eb88
Gtrakas1 <gtrakas23@gmail.com>
2019-09-04 17:35:13 -0500

    Add files via upload

---
+++ Workshop1/Workshop1/w1_home.c
@@ -0,0 +1,14 @@
+/*Welcome to C Programming
+Gtrakas1*/
+
+#include<stdio.h> //information about the printf identifier
+
+int main(void)  // the starting point of the program
+
+{
+	// code block upgrade
+		printf("******************************\n");
+			printf("** Welcome to C Programming **\n");
+				printf("******************************\n");
+			return 0;  //return to operating system
+}
